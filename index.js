4eed57e41fdc1fe51e25f40de7e6274273cdd2b9
ashutosh <ashutoshdewangan5@gmail.com>
2023-10-07 11:31:02 +0530

    commit

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
-// Function to register a user (simplified without server interaction)
+
 function registerUser() {
     const regUsername = document.getElementById("regUsername").value;
     const regPassword = document.getElementById("regPassword").value;
   
-    // Store the user data securely (not implemented here)
-  
     alert("Registration successful!");
-    return false; // Prevent form submission
+    return false; 
   }
   
-  // Function to login a user (simplified without server interaction)
   function loginUser() {
     const loginUsername = document.getElementById("loginUsername").value;
     const loginPassword = document.getElementById("loginPassword").value;
   
-    // Authenticate user (not implemented here)
-  
-    // If authentication is successful, show secured content
     document.getElementById("login-form").style.display = "none";
     document.getElementById("secured-content").style.display = "block";
   
-    return false; // Prevent form submission
+    return false; 
   }
   
-  // Function to logout a user
   function logoutUser() {
-    // Clear session or perform necessary logout actions
+   
     document.getElementById("login-form").style.display = "block";
     document.getElementById("secured-content").style.display = "none";
   }
@@ -38,7 +31,7 @@ function registerUser() {
     if (registrationForm.style.display === "none" || registrationForm.style.display === "") {
       registrationForm.style.display = "block";
       loginForm.style.display = "none";
-      toggleForms.textContent = "Login page";
+      toggleForms.textContent = "New to Authentication? Create account";
     } else {
       registrationForm.style.display = "none";
       loginForm.style.display = "block";
